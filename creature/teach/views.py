873a76869a4ebf50bded11f12149e38b36e7bf7e
Daniel Fordham <dfordham46@gmail.com>
2024-09-16 23:57:22 -0400

    real time now

--- creature/teach/views.py
+++ creature/teach/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from pyexpat import model
+from django.views.generic.detail import DetailView
+
+from django.shortcuts import render, redirect
+#from .models import  MODELS HERE
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
